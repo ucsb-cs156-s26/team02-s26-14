3d9f18cd3351b0c6857df2c84b152e3071524e0f
Alessandro Ballerini <alessandro_ballerini@ucsb.edu>
2026-05-01 19:09:55 -0700

    ale - create fixtures for commit table

---
+++ frontend/src/fixtures/articleFixtures.js
@@ -0,0 +1,33 @@
+const  articleFixtures= {
+  oneArticle: {
+    
+  },
+  threeArticles: [
+   {
+    "id": 2,
+    "title": "complete guide to github",
+    "url": "https://docs.github.com/en/get-started",
+    "explanation": "Official GitHub documentation for beginners",
+    "email": "cgaucho@ucsb.edu",
+    "dateAdded": "2026-05-01T10:23:45"
+  },
+  {
+    "id": 3,
+    "title": "complete guide to python",
+    "url": "https://docs.python.org/3/tutorial/",
+    "explanation": "Official python documentation for beginners",
+    "email": "cgaucho@ucsb.edu",
+    "dateAdded": "2026-05-01T10:23:45"
+  },
+  {
+    "id": 4,
+    "title": "complete guide to react",
+    "url": "https://react.dev/learn",
+    "explanation": "Official react documentation for beginners",
+    "email": "cgaucho@ucsb.edu",
+    "dateAdded": "2026-05-01T10:23:45"
+  }
+  ],
+};
+
+export { articleFixtures };
