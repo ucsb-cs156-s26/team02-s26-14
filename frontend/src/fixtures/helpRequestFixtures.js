2462adb2fd884ef504ae110c527714b9a6cead8e
VishvaAr <varasan@ucsb.edu>
2026-05-03 19:18:08 -0600

    Complete HelpRequestForm with 100% mutation coverage

--- frontend/src/fixtures/helpRequestFixtures.js
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -1,30 +1,42 @@
-const ucsbDatesFixtures = {
-  oneDate: {
-    id: 1,
-    quarterYYYYQ: "20221",
-    name: "Noon on January 2nd",
-    localDateTime: "2022-01-02T12:00:00",
-  },
-  threeDates: [
-    {
-      id: 1,
-      quarterYYYYQ: "20221",
-      name: "Noon on January 2nd",
-      localDateTime: "2022-01-02T12:00:00",
+const helpRequestFixtures = {
+    oneHelpRequest: {
+        "id": 1,
+        "requesterEmail": "cgaucho@ucsb.edu",
+        "teamId": "s22-5pm-3",
+        "tableOrBreakoutRoom": "7",
+        "requestTime": "2022-04-20T17:35:00",
+        "explanation": "Need help with Swagger UI",
+        "solved": false
     },
-    {
-      id: 2,
-      quarterYYYYQ: "20222",
-      name: "Noon on April 3rd",
-      localDateTime: "2022-04-03T12:00:00",
-    },
-    {
-      id: 3,
-      quarterYYYYQ: "20223",
-      name: "Noon on July 4th",
-      localDateTime: "2022-07-04T12:00:00",
-    },
-  ],
+    threeHelpRequests: [
+        {
+            "id": 1,
+            "requesterEmail": "cgaucho@ucsb.edu",
+            "teamId": "s22-5pm-3",
+            "tableOrBreakoutRoom": "7",
+            "requestTime": "2022-04-20T17:35:00",
+            "explanation": "Need help with Swagger UI",
+            "solved": false
+        },
+        {
+            "id": 2,
+            "requesterEmail": "ldelplaya@ucsb.edu",
+            "teamId": "s22-6pm-3",
+            "tableOrBreakoutRoom": "11",
+            "requestTime": "2022-04-20T18:31:00",
+            "explanation": "Heroku deployment failed",
+            "solved": true
+        },
+        {
+            "id": 3,
+            "requesterEmail": "pdante@ucsb.edu",
+            "teamId": "s22-6pm-4",
+            "tableOrBreakoutRoom": "13",
+            "requestTime": "2022-04-21T14:15:00",
+            "explanation": "Merge conflict in PR",
+            "solved": false
+        }
+    ]
 };
 
-export { ucsbDatesFixtures };
+export { helpRequestFixtures };
