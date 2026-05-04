e814f8a70f83ed584294d888be2e8ddb057a36d2
VishvaAr <varasan@ucsb.edu>
2026-05-03 21:04:47 -0600

    fix: update fixtures to use HelpRequest data

--- frontend/src/fixtures/helpRequestFixtures.js
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -1,30 +1,42 @@
-const ucsbDatesFixtures = {
-  oneDate: {
-    id: 1,
-    quarterYYYYQ: "20221",
-    name: "Noon on January 2nd",
-    localDateTime: "2022-01-02T12:00:00",
-  },
-  threeDates: [
-    {
-      id: 1,
-      quarterYYYYQ: "20221",
-      name: "Noon on January 2nd",
-      localDateTime: "2022-01-02T12:00:00",
+const helpRequestFixtures = {
+    oneHelpRequest: {
+        "id": 1,
+        "requesterEmail": "cgaucho@ucsb.edu",
+        "teamId": "s22-5pm-3",
+        "tableOrBreakoutRoom": "7",
+        "requestTime": "2022-04-20T17:35:00",
+        "explanation": "Need help with Swagger",
+        "solved": false
     },
-    {
-      id: 2,
-      quarterYYYYQ: "20222",
-      name: "Noon on April 3rd",
-      localDateTime: "2022-04-03T12:00:00",
-    },
-    {
-      id: 3,
-      quarterYYYYQ: "20223",
-      name: "Noon on July 4th",
-      localDateTime: "2022-07-04T12:00:00",
-    },
-  ],
+    threeHelpRequests: [
+        {
+            "id": 1,
+            "requesterEmail": "ldelplaya@ucsb.edu",
+            "teamId": "s22-6pm-3",
+            "tableOrBreakoutRoom": "11",
+            "requestTime": "2022-04-20T18:31:00",
+            "explanation": "Heroku deployment issues",
+            "solved": false
+        },
+        {
+            "id": 2,
+            "requesterEmail": "phtcon@ucsb.edu",
+            "teamId": "s22-4pm-1",
+            "tableOrBreakoutRoom": "13",
+            "requestTime": "2022-04-20T18:35:00",
+            "explanation": "Merge conflict in Team02",
+            "solved": false
+        },
+        {
+            "id": 3,
+            "requesterEmail": "amogus@ucsb.edu",
+            "teamId": "s22-5pm-4",
+            "tableOrBreakoutRoom": "10",
+            "requestTime": "2022-04-20T18:37:00",
+            "explanation": "Tests not passing",
+            "solved": true
+        }
+    ]
 };
 
-export { ucsbDatesFixtures };
+export { helpRequestFixtures };
