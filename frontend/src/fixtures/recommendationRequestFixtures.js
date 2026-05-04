fffd2c6fbd9f93ee42d4af44532b5e734b375c07
Abhijeet Dutta <abhijeet@ucsb.edu>
2026-05-03 18:27:44 -0700

    added recreq fixtures

---
+++ frontend/src/fixtures/recommendationRequestFixtures.js
@@ -0,0 +1,42 @@
+const recommendationRequestFixtures = {
+  oneRequest: {
+    "id": 2,
+    "requesterEmail": "abhijeet@ucsb.edu",
+    "professorEmail": "abhijeet@ucsb.edu",
+    "explanation": "hi",
+    "dateRequested": "2026-05-03T20:20:21",
+    "dateNeeded": "2026-05-03T20:20:22",
+    "done": false
+  },
+  threeRequests: [
+  {
+    "id": 2,
+    "requesterEmail": "abhijeet@ucsb.edu",
+    "professorEmail": "abhijeet@ucsb.edu",
+    "explanation": "hi",
+    "dateRequested": "2026-05-03T20:20:21",
+    "dateNeeded": "2026-05-03T20:20:22",
+    "done": false
+  },
+  {
+    "id": 3,
+    "requesterEmail": "abhijeet@ucsb.edu",
+    "professorEmail": "abhijeet@ucsb.edu",
+    "explanation": "seeya",
+    "dateRequested": "2026-05-03T20:20:22",
+    "dateNeeded": "2026-05-03T20:20:23",
+    "done": false
+  },
+  {
+    "id": 4,
+    "requesterEmail": "abhijeet@ucsb.edu",
+    "professorEmail": "abhijeet@ucsb.edu",
+    "explanation": "bye",
+    "dateRequested": "2026-05-03T20:20:23",
+    "dateNeeded": "2026-05-03T20:20:24",
+    "done": true
+  }
+],
+};
+
+export { ucsbDatesFixtures };
