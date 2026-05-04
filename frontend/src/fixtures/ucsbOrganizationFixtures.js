c13b8c60a8a068f72c2ac1dab6d50535540d4269
Benny Zhang <luandayouzhang@ucsb.edu>
2026-05-04 11:34:59 -0700

    create fixture of UCSBOrganization

---
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -0,0 +1,35 @@
+const ucsbOrganizationFixtures = {
+  oneOrganization: [
+    {
+        orgCode: "ACM",
+        orgTranslationShort: "ACM",
+        orgTranslation: "Association for Computing Machinery",
+        inactive: false,
+    },
+  ],
+
+  threeOrganizations: [
+    {
+        orgCode: "ACM",
+        orgTranslationShort: "ACM",
+        orgTranslation: "Association for Computing Machinery",
+        inactive: false,
+    },
+
+    {
+      orgCode: "IEEE",
+      orgTranslationShort: "IEEE",
+      orgTranslation: "Institute of Electrical and Electronics Engineers",
+      inactive: false,
+    },
+
+    {
+      orgCode: "DSC",
+      orgTranslationShort: "Data Science Club",
+      orgTranslation: "Data Science Club",
+      inactive: true,
+    },
+  ],
+};
+
+export {  ucsbOrganizationFixtures };
