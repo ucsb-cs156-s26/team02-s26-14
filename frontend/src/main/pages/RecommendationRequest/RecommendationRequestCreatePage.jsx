51bf569a00bdfe74acd2eebd8415cddd084aa8e3
Abhijeet Dutta <abhijeet@ucsb.edu>
2026-05-05 15:22:52 -0700

    abhi - made create page logic

--- frontend/src/main/pages/RecommendationRequest/RecommendationRequestCreatePage.jsx
+++ frontend/src/main/pages/RecommendationRequest/RecommendationRequestCreatePage.jsx
@@ -1,11 +1,52 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import RecommendationRequestForm from "main/components/RecommendationRequest/RecommendationRequestForm";
+import { Navigate } from "react-router";
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function RecommendationRequestCreatePage() {
-  // Stryker disable all : placeholder for future implementation
+export default function RecommendationRequestCreatePage({ storybook = false }) {
+
+  const addZ = (string) => `${string}Z`;
+  const objectToAxiosParams = (recommendationRequest) => ({
+    url: "/api/recommendationrequests/post",
+    method: "POST",
+    params: {
+    requesterEmail: recommendationRequest.requesterEmail,
+    professorEmail: recommendationRequest.professorEmail,
+    explanation: recommendationRequest.explanation,
+    dateRequested: addZ(recommendationRequest.dateRequested),
+    dateNeeded: addZ(recommendationRequest.dateNeeded),
+    done: recommendationRequest.done,
+    },
+  });
+
+  const onSuccess = (recommendationRequest) => {
+    toast(`New Recommendation Request Created - id: ${recommendationRequest.id} name: ${recommendationRequest.name}`);
+  };
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    ["/api/recommendationrequests/all"],
+  );
+
+  const { isSuccess } = mutation;
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  };
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/recommendationrequests" />;
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Create page not yet implemented</h1>
+        <h1>Create New Recommendation Request</h1>
+
+        <RecommendationRequestForm submitAction={onSubmit} />
       </div>
     </BasicLayout>
   );
