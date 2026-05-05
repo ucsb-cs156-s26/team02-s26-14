b60304c08ffc069d3f85f16b4d5efd1b992e19d8
Brian Ridder <brianridder@ucsb.edu>
2026-05-05 06:37:23 -0700

    menuItemReviewFixtures.js file created, with one and three sample menuItemReviews

---
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -0,0 +1,43 @@
+const menuItemReviewFixtures = {
+  oneMenuItemReview: [
+    {
+      id: 1,
+      itemId: 67,
+      reviewerEmail: "bigchungus@ucsb.edu",
+      stars: 5,
+      dateReviewed: "2024-05-05 10:30:45",
+      comments: "I loved the beans, so tasty!"
+    },
+  ],
+
+  threeMenuItemReviews: [
+    {
+      id: 2,
+      itemId: 68,
+      reviewerEmail: "lilchungus@ucsb.edu",
+      stars: 3,
+      dateReviewed: "2024-05-05 10:35:45",
+      comments: "It was alright, nothing special"
+    },
+    
+    {
+      id: 3,
+      itemId: 69,
+      reviewerEmail: "averagesizedchungus@ucsb.edu",
+      stars: 4,
+      dateReviewed: "2024-05-05 10:40:45",
+      comments: "I really like this, but I've grown a little sick of it."
+    },
+
+    {
+      id: 4,
+      itemId: 70,
+      reviewerEmail: "biggestchungus@ucsb.edu",
+      stars: 1,
+      dateReviewed: "2024-05-05 10:30:45",
+      comments: "I only eat organic, locally farmed foods. I wouldn't ever eat this!"
+    },
+  ],
+};
+
+export { menuItemReviewFixtures };
